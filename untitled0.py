1eac62402479f29ccc926d592a7f5278c75162af
Юлия <103586085+mamedovau28@users.noreply.github.com>
2025-03-07 12:32:36 +0300

    untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -7,8 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1xJ67WECHx06qFmacBoK_9SgrMNac7eJf
 """
 
-pip cache purge
-pip install --no-cache-dir openpyxl
+pip install openpyxl
 
 import streamlit as st
 import pandas as pd

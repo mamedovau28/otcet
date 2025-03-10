e1e6032fa39ad9928d8f48537c6bc1eadf76bdc1
Юлия <103586085+mamedovau28@users.noreply.github.com>
2025-03-10 19:26:50 +0300

    untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -97,10 +97,10 @@ if mp_file and metki_file:
             return pd.NaT, pd.NaT
             
 # Применение функции и создание новых столбцов с начальной и конечной датой
-if 'Период' in df.columns:
-    df[['Start Date', 'End Date']] = df['Период'].apply(extract_dates).apply(pd.Series)
-else:
-    st.error("Столбец 'Период' не найден в данных.")
+    if 'Период' in df.columns:
+        df[['Start Date', 'End Date']] = df['Период'].apply(extract_dates).apply(pd.Series)
+    else:
+        st.error("Столбец 'Период' не найден в данных.")
 
 # Бюджет по неделям
 def calculate_budget_per_week(row):

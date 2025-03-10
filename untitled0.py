fb6b961c9ffe35ad5f665e838d136fe71bd79d1d
Юлия <103586085+mamedovau28@users.noreply.github.com>
2025-03-10 15:49:29 +0300

    untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -367,6 +367,9 @@ def get_work_done(report_start, report_end):
 
 work_done_list = get_work_done(report_start, report_end)
 
+st.markdown("### Проделанные работы:")
+st.markdown("\n".join(f"- {work}" for work in work_done_list))
+
     # Генерация отчёта
     report_text = f"""
     Медийная реклама ({report_start.strftime('%d.%m.%y')}-{report_end.strftime('%d.%m.%y')})

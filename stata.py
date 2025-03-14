e655d92dcc4a240fc347f9370a6035ae239ad36b
Юлия <103586085+mamedovau28@users.noreply.github.com>
2025-03-14 14:48:17 +0300

    stata.py

--- stata.py
+++ stata.py
@@ -1,4 +1,3 @@
-> Юлия Мамедова:
 import streamlit as st
 import pandas as pd
 import re
@@ -101,3 +100,50 @@ elif upload_option == "Ссылки на Google-таблицы":
                 df = pd.read_csv(csv_url)
                 dfs.append(df)
                 campaign_name = extract_campaign_name_from_gid(google_sheet_url)
+                campaign_names.append(campaign_name)
+            except Exception as e:
+                st.error(f"Ошибка при загрузке CSV: {e}")
+
+if dfs:
+    for df, campaign_name in zip(dfs, campaign_names):
+        df, col_map = process_data(df)
+        st.write(f"Название РК: {campaign_name}")
+
+        # Выбор периода
+        if "дата" in col_map:
+            min_date = df[col_map["дата"]].min().date()
+            max_date = df[col_map["дата"]].max().date()
+            start_date, end_date = st.date_input("Выберите период", [min_date, max_date])
+
+            df_filtered = df[
+                (df[col_map["дата"]].dt.date >= start_date) & 
+                (df[col_map["дата"]].dt.date <= end_date)
+            ]
+
+            needed_cols = ["показы", "клики", "охват", "расход с ндс"]
+            
+            existing_cols = [col for col in needed_cols if col in df_filtered.columns]
+            summary = df_filtered[existing_cols].sum()
+
+            total_impressions = summary.get("показы", 0)
+            total_clicks = summary.get("клики", 0)
+            ctr_value = total_clicks / total_impressions if total_impressions > 0 else 0
+            total_reach = summary.get("охват", 0)
+            total_spend_nds = summary.get("расход с ндс", 0)
+
+            # Генерация отчёта
+            report_text = f"""
+            {campaign_name}
+        Показы: {format(total_impressions, ",.0f").replace(",", " ")}
+        Клики: {format(total_clicks, ",.0f").replace(",", " ")}
+        CTR: {ctr_value:.2%}
+        Охват: {format(total_reach, ",.0f").replace(",", " ")}
+        Расход с НДС: {format(total_spend_nds, ",.2f").replace(",", " ")} руб.
+            """
+
+            # Вывод отчёта
+            st.subheader(f"Итоговый отчёт для {campaign_name}")
+            st.text_area(report_text, report_text, height=100)
+
+        # Вывод таблицы
+        st.dataframe(df)

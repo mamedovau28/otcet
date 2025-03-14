91ee0216891e3bb4e8272f26cfd8ef3792ec4c47
Юлия <103586085+mamedovau28@users.noreply.github.com>
2025-03-14 18:08:51 +0300

    stata.py

--- stata.py
+++ stata.py
@@ -191,11 +191,11 @@ for i in range(1, 11):
             # Генерация отчёта
             report_text = f"""
             {custom_campaign_name}
-        Показы: {format(total_impressions, ",.0f").replace(",", " ")}
-        Клики: {format(total_clicks, ",.0f").replace(",", " ")}
-        CTR: {ctr_value:.2%}
-        Охват: {format(total_reach, ",.0f").replace(",", " ")}
-        Расход с НДС: {format(total_spend_nds, ",.2f").replace(",", " ")} руб.
+            Показы: {format(total_impressions, ",.0f").replace(",", " ")}
+            Клики: {format(total_clicks, ",.0f").replace(",", " ")}
+            CTR: {ctr_value:.2%}
+            Охват: {format(total_reach, ",.0f").replace(",", " ")}
+            Расход с НДС: {format(total_spend_nds, ",.2f").replace(",", " ")} руб.
             """
 
             # Вывод отчёта

9afb0ccba7d60409dc3e2ca50ca7bb00dc5403a0
Юлия <103586085+mamedovau28@users.noreply.github.com>
2025-03-14 14:29:19 +0300

    stata.py

--- stata.py
+++ stata.py
@@ -129,12 +129,12 @@ if df is not None:
 
         # Генерация отчёта
         report_text = f"""
-        {campaign_name}
-    Показы: {format(total_impressions:",.0f").replace(",", " ")}
-    Клики: {format(total_clicks:",.0f").replace(",", " ")}
+        Название РК: {campaign_name}
+    Показы: {format(total_impressions, ",.0f").replace(",", " ")}
+    Клики: {format(total_clicks, ",.0f").replace(",", " ")}
     CTR: {ctr_value:.2%}
-    Охват: {format(total_reach:",.0f").replace(",", " ")}
-    Расход с НДС: {format(total_spend_nds:",.0f").replace(",", " ")} руб.
+    Охват: {format(total_reach, ",.0f").replace(",", " ")}
+    Расход с НДС: {format(total_spend_nds, ",.2f").replace(",", " ")}
         """
 
         # Вывод отчёта

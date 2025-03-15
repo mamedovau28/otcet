6dd3f6eb80bd3ab66bec8eb739e8d36dd6f3c4ab
Юлия <103586085+mamedovau28@users.noreply.github.com>
2025-03-15 18:29:47 +0300

    stata.py

--- stata.py
+++ stata.py
@@ -438,10 +438,8 @@ for i in range(1, 11):
 
         if mp_df is not None:  # Если медиаплан был загружен
             match_message = check_matching_campaign(mp_df, custom_campaign_name)
-            # Анализируем кампанию
-            result = analyze_campaign(mp_df)  # Используем данные для анализа кампании
             st.write(match_message)
-            st.write(result)
+            st.write(analyze_campaign)
             
         if "дата" in col_map:
             min_date = df[col_map["дата"]].min().date()

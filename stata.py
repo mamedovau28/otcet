9f0fa25169a916297fd47e1a74f11bacc8455328
Юлия <103586085+mamedovau28@users.noreply.github.com>
2025-03-16 17:38:40 +0300

    stata.py

--- stata.py
+++ stata.py
@@ -512,35 +512,32 @@ for i in range(1, num_uploads + 1):
     df = None
     campaign_name = None
 
-# Применяем CSS для изменения фона и уменьшения ширины
-st.markdown("""
-    <style>
-        /* Изменяем стиль для полей загрузки файлов */
-        .stFileUploader div {
-            background-color: #f0f8e0;  /* светло-салатовый цвет */
-            width: 50%;  /* Уменьшаем ширину в два раза */
-            padding: 5px;
-        }
+    # Применяем CSS для изменения фона и уменьшения ширины
+    st.markdown("""
+        <style>
+            /* Изменяем стиль для полей загрузки файлов */
+            .stFileUploader div {
+                background-color: #f0f8e0;  /* светло-салатовый цвет */
+                padding: 5px;
+            }
         
-        /* Изменяем стиль для текстового ввода для Google-таблиц */
-        .stTextInput input {
-            background-color: #f0f8e0;  /* светло-салатовый цвет */
-            width: 50%;  /* Уменьшаем ширину в два раза */
-        }
+            /* Изменяем стиль для текстового ввода для Google-таблиц */
+            .stTextInput input {
+                background-color: #f0f8e0;  /* светло-салатовый цвет */
+            }
 
-        /* Применяем стили ко всем полям ввода (как для числовых, так и для текстовых) */
-        .stNumberInput input,
-        .stTextInput input,
-        .stFileUploader div,
-        .stSelectbox select,
-        .stTextArea textarea {
-            background-color: #f0f8e0;  /* светло-салатовый цвет */
-            width: 50%;  /* Уменьшаем ширину */
-            margin: 10px 0;
-            padding: 5px;
-        }
-    </style>
-""", unsafe_allow_html=True)
+            /* Применяем стили ко всем полям ввода (как для числовых, так и для текстовых) */
+            .stNumberInput input,
+            .stTextInput input,
+            .stFileUploader div,
+            .stSelectbox select,
+            .stTextArea textarea {
+                background-color: #f0f8e0;  /* светло-салатовый цвет */
+                margin: 10px 0;
+                padding: 5px;
+            }
+        </style>
+    """, unsafe_allow_html=True)
 
 
     if upload_option == "Загрузить Excel-файл":
